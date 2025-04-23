52559dc08eaf31f35a098febc673a92e89deb96c
Kesomannen <bobbo.nidor@gmail.com>
2025-04-23 18:59:24 +0200

    real recipe

--- src/commands/hakan/recipe.rs
+++ src/commands/hakan/recipe.rs
@@ -1,5 +1,5 @@
 use serenity::all::{
-    CommandInteraction, Context, CreateCommand, CreateEmbed, CreateInteractionResponse,
+    Color, CommandInteraction, Context, CreateCommand, CreateEmbed, CreateInteractionResponse,
     CreateInteractionResponseMessage, RoleId,
 };
 
@@ -9,7 +9,27 @@ pub fn register() -> CreateCommand {
     CreateCommand::new("håkanrecept").description("Visa håkanreceptet.")
 }
 
-const RECIPE: &'static str = "Slib slorb";
+const RECIPE: &'static str = "
+Ca 48 bitar.
+
+God som en liten smakbit efter maten.
+Gärna med en klick vispgrädde till.
+
+8 ägg
+2 ½ dl kakao
+10 dl (850 g) strösocker
+6 dl (500 g) vetemjöl
+1 tsk salt
+400 g smält smör
+
+1. Sätt ugnen på 175°.
+2. Vispa äggen och sockret pösigt.
+3. Rör ner de övriga ingredienserna.
+4. Smörj en långpanna och häll upp smeten.
+5. Grädda kakan på nedersta falsen i ugnen i ca 35 minuter. Känn efter med en stick när kakan är torr.
+6. Skär upp kakan i 5 x 5 cm stora rutor.
+
+Njut 😋";
 
 #[tracing::instrument]
 pub async fn run(
@@ -24,6 +44,7 @@ pub async fn run(
                 CreateInteractionResponseMessage::new().add_embed(
                     CreateEmbed::new()
                         .title("🍰 Håkanrecept")
+                        .color(Color::DARK_GREEN)
                         .description(RECIPE),
                 ),
             ),
